0f199a2081134555051a442fa0772f6a5e9cffb5
sanz <sanz@crahi.upc.edu>
2025-05-23 13:12:29 +0200

    Save messages as Madrid tz

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, Text, DateTime
 from datetime import datetime
 from .database import Base
+from zoneinfo import ZoneInfo
 
 class Message(Base):
     __tablename__ = "messages"
     id = Column(Integer, primary_key=True, index=True)
     text = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=datetime.now)
+    created_at = Column(DateTime, default=datetime.now(tz=ZoneInfo("Europe/Madrid")))
     author = Column(Text, nullable = False)
