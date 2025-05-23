a574f2ff1be604b2042a536151984e56cd743828
sanz <sanz@crahi.upc.edu>
2025-05-23 10:38:59 +0200

    Change schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class MessageCreate(BaseModel):
     text: str
@@ -7,7 +8,7 @@ class MessageCreate(BaseModel):
 class MessageOut(BaseModel):
     id: int
     text: str
-    created_at: str
+    created_at: datetime
     author: str
 
     class Config:
